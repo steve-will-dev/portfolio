266b6b4bf37d3f536c1686425da38fe6e10c6c5c
stevelikeafox <stephenwillmot@googlemail.com>
2022-04-12 00:03:53 +0100

    test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
     console.log('New Connection')
-    res.end('hello world - CD working')
+    res.end('hello world - Git CD')
 })
 const PORT = process.env.PORT || 8080
-server.listen(PORT, () => console.log('Listening on ', {PORT}))
+server.listen(PORT, () => console.log('Listening on ', { PORT }))
